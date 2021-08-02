8fae555e7ba091ceeaa0795db29eeb067e8b7d3d
raclim <43053081+raclim@users.noreply.github.com>
2021-08-02 14:59:24 -0400

    favicon to link

--- pages/[slug].js
+++ pages/[slug].js
@@ -56,7 +56,7 @@ export default function Project({portfolioWork}) {
         <div className={styles.homeContainer}>
             <Head>
                 <title>RACLIM ONLINE | GITHUB</title>
-                <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+                <link rel="shortcut icon" type="image/x-icon" href="https://raclim.cool/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet" />
