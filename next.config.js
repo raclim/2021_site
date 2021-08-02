fa9cb1bf00610a4c42e0060953e0287a57644e7a
raclim <43053081+raclim@users.noreply.github.com>
2021-08-02 13:15:52 -0400

    deploy updates

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
     images: {
         domains: ['images.ctfassets.net']
-    }
+    },
+    assetPrefix: isProd ? '/raclim.github.io/' : ''
 }
